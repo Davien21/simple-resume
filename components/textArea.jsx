0f4318c466d7c5e16f55f48183262c9e045714e6
davien21 <chidiebereekennia@gmail.com>
2021-08-19 20:46:02 +0100

    add text area component

---
+++ components/textArea.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TextArea = ({ name, label, error, ...rest }) => {
+  const textAreaClassName = !error ? "default_style" : "default_style error";
+  const errorClassName = !error ? "" : "error-message";
+
+  return (
+    <div className="form-group mt-2">
+      <textarea
+        {...rest}
+        name={name}
+        placeholder={label}
+        id={name}
+        rows="2"
+        className={textAreaClassName}
+      ></textarea>
+      {error && <div className={errorClassName}>{error}</div>}
+    </div>
+  );
+};
+
+export default TextArea;
