372d2b3bdf54432425f5992044d02221f9fb82a7
davien21 <chidiebereekennia@gmail.com>
2021-08-19 20:37:29 +0100

    add all images

---
+++ images.js
@@ -0,0 +1,20 @@
+import twitterIcon from "./public/imgs/svgs/twitter.svg";
+import githubIcon from "./public/imgs/svgs/github.svg";
+import linkedinIcon from "./public/imgs/svgs/linkedin.svg";
+import logos from "./public/imgs/svgs/logos.svg";
+import hngLogos from "./public/imgs/svgs/hngLogo.svg";
+import rightArrowIcon from "./public/imgs/svgs/right-chevron.svg";
+import menuIcon from "./public/imgs/svgs/menu.svg";
+
+const hngLogo = "/imgs/hng.png";
+
+module.exports = {
+  twitterIcon,
+  githubIcon,
+  linkedinIcon,
+  hngLogo,
+  hngLogos,
+  logos,
+  rightArrowIcon,
+  menuIcon,
+};
