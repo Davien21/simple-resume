737a9d3a507d8f573b1e5e8286386aba62de552a
davien21 <chidiebereekennia@gmail.com>
2021-08-19 20:46:36 +0100

    add contact form

---
+++ components/forms/contact.jsx
@@ -0,0 +1,51 @@
+import Form from "./index";
+import Joi from "joi-browser";
+
+class ContactForm extends Form {
+  state = {
+    data: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    errors: {},
+  };
+
+  schema = {
+    id: Joi.string(),
+    name: Joi.string().required().label("Name"),
+    email: Joi.string().email().required().label("Email Address"),
+    message: Joi.string().required().label("Message"),
+  };
+
+  async componentDidMount() {}
+
+  async openEmployeePage(id) {
+    this.props.history.push(`/employees/listings/${id}`);
+  }
+  mayalpoq;
+  doSubmit = () => {
+    console.log("Success!");
+  };
+
+  render() {
+    return (
+      <div>
+        <p className="title mb-4 semi-bold">GET IN TOUCH</p>
+        <form
+          onSubmit={this.handleSubmit}
+          action={this.state.errors.length ?? "https://formspree.io/f/mayalpoq"}
+          id="contact-form"
+          method="POST"
+        >
+          {this.renderInput("name", "Your Name *")}
+          {this.renderInput("email", "Email *")}
+          {this.renderTextArea("message", "Message *")}
+          {this.renderButton("Send Message")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
