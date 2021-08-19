10b420eef5f034e21029d29bb124ce2cf2e790b1
davien21 <chidiebereekennia@gmail.com>
2021-08-19 20:47:11 +0100

    add contact page

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "./../components/forms/contact";
+
+function ContactPage(props) {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default ContactPage;
