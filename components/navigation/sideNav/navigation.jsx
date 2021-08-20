9b150ca21b9062992eecf4a862f47640f66f268a
davien21 <chidiebereekennia@gmail.com>
2021-08-20 16:01:12 +0100

    add social media and logo

--- components/navigation/sideNav/navigation.jsx
+++ components/navigation/sideNav/navigation.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable @next/next/link-passhref */
 import { motion } from "framer-motion";
 import { CloseButton } from "./../../closeButton";
 import Menu from "./../../menu";
+import TwitterImage from "./../../icons/twitter";
+import GithubImage from "./../../icons/github";
+import LinkedinImage from "./../../icons/linkedin";
+import Link from "next/link";
 
 const slideIn = {
   left: "0",
@@ -19,10 +24,18 @@ function SideBar({ isOpen, onCloseSideBar }) {
       animate={isOpen ? slideIn : slideOut}
       className="side-nav"
     >
-      <div className="d-flex my-2">
+      <div className=" d-flex my-2">
+        <Link href="/" >
+          <span className="logo">Chidiebere</span>
+        </Link>
         <CloseButton toggle={onCloseSideBar} />
       </div>
       <Menu />
+      <div className="w-80 d-flex justify-content-between social-media">
+        <TwitterImage />
+        <GithubImage />
+        <LinkedinImage />
+      </div>
     </motion.div>
   );
 }
