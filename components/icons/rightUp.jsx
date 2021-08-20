184c2e248b62b7ad7c06da3d729a658941a298ad
davien21 <chidiebereekennia@gmail.com>
2021-08-20 15:00:16 +0100

    add right-up icon

---
+++ components/icons/rightUp.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import React from "react";
+import { rightUpIcon } from "../../images";
+
+function RightUpImage(props) {
+  return <Image alt="" src={rightUpIcon} width={12} height={12} />;
+}
+
+export default RightUpImage;
