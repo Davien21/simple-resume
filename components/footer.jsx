151391d872e328b3a9930d5d0ede58addd238940
davien21 <chidiebereekennia@gmail.com>
2021-08-21 20:28:07 +0100

    minor style updates

--- components/footer.jsx
+++ components/footer.jsx
@@ -13,11 +13,11 @@ function Footer(props) {
           <div className="row align-items-center">
             <span className="col-auto mt-3 mt-md-0  title">Social Media:</span>
             <div className="col col-lg-auto px-0 mt-3 mt-md-0">
-              <div className="d-flex justify-content-between">
+              <div className="d-flex ">
                 <div className="col-auto  ">
                   <TwitterImage />
                 </div>
-                <div className="col-auto px-0">
+                <div className="col-auto">
                   <GithubImage />
                 </div>
                 <div className="col-auto ">
@@ -25,26 +25,14 @@ function Footer(props) {
                 </div>
               </div>
             </div>
-            <div className="d-flex align-items-center logo-div ml-md-auto mt-4 mt-md-0 col-auto  ">
-              <Image
-                src={logos}
-                alt="Zuri / I4G Logo"
-                width={238}
-                height={59}
-              />
-            </div>
           </div>
         </div>
         <ContactButton />
       </div>
       <div className="bottom mt-3">
-        <p className="">Developed with ♥ by Me for HNG</p>
-        {/* <div className="col-auto">
-          <Image src={logos} alt="Zuri / I4G Logo" width={250} height={62} />
-        </div> */}
-        {/* <div className="col-auto">
-          <Image src={hngLogo} alt="HNG Logo" width={100} height={100} />
-        </div> */}
+        <p className="">
+          Developed with <span className="">♥</span> by me
+        </p>
       </div>
     </footer>
   );
