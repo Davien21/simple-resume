8e83125f731e3af63ff2ca541057ac0af8ccac17
davien21 <chidiebereekennia@gmail.com>
2021-09-28 08:30:10 +0100

    update tab size

--- components/segmentedControl/index.jsx
+++ components/segmentedControl/index.jsx
@@ -1,22 +1,26 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import styles from "./segmentedControl.module.css";
 import { motion } from "framer-motion";
 
 function SegmentedControl({ active, controls, onSetActiveControl }) {
-  const sliderWidth = Math.floor((1 / controls.length) * 100) - 2 + "%";
-  const sliderLeftPosition =
-    Math.ceil((controls.indexOf(active) / controls.length) * 100) + 1 + "%";
+  const sliderWidth = Math.floor((1 / controls.length) * 100) + "%";
+  const sliderLeftPosition = (() => {
+    const activeIndex = controls.indexOf(active);
+    return Math.round((activeIndex / controls.length) * 100) + "%";
+  })();
 
   const controlClasses = (control) =>
     control == active ? `${styles.active}` : "";
 
   return (
     <ul className={styles.container}>
-      <motion.div
-        initial={{ left: 0, width: sliderWidth }}
-        animate={{ left: sliderLeftPosition }}
-        className={styles.slider}
-      ></motion.div>
+      <div className={styles["slider-container"]}>
+        <motion.div
+          initial={{ left: 0, width: sliderWidth }}
+          animate={{ left: sliderLeftPosition }}
+          className={styles.slider}
+        ></motion.div>
+      </div>
       {controls.map((control) => (
         <li
           onClick={() => onSetActiveControl(control)}
