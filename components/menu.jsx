75de60f8363ee508c84a79511a03c3f9de620b2b
davien21 <chidiebereekennia@gmail.com>
2021-11-29 20:47:45 +0100

    update resume

--- components/menu.jsx
+++ components/menu.jsx
@@ -9,7 +9,7 @@ function Menu(props) {
       <ul>
         <NavLink href="/">About</NavLink>
 
-        {/* <NavLink href="/work">Work</NavLink> */}
+        <NavLink href="/work">Work</NavLink>
 
         <NavLink href="/experience">Experience</NavLink>
 
@@ -17,7 +17,7 @@ function Menu(props) {
 
         <li>
           <ExternalLink
-            href="https://drive.google.com/file/d/1WsTn1FF8WxBmM-di0bXLJ1bslmUzyZhC/view?usp=sharing"
+            href="https://drive.google.com/file/d/1S0myL39LNDFXdpuZQLh1GNNeBN4hx3hK/view?usp=sharing"
             className="article-title"
           >
             <span className="pr-3">Resume</span>
@@ -31,7 +31,7 @@ function Menu(props) {
             href="https://docs.google.com/document/d/1mOgn7CP1zsRRNLGc2eRUn_b_cvmVIi1FAbnlrVT9Pns/edit?usp=sharing"
             className="article-title"
           >
-            <span className="pr-3">Project Catalog</span>
+            <span className="pr-3">Catalog</span>
 
             <RightUpImage />
           </ExternalLink>
