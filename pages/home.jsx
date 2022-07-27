ebaed0ca5e5f933bc314bb5515dc93948ad916cb
chidi-locusnine <chidiebere@locusnine.com>
2022-07-27 22:01:36 +0100

    --feat: add scroll-down indicator

--- pages/home.jsx
+++ pages/home.jsx
@@ -35,10 +35,47 @@ export default function Home() {
         <div className="profile-image position-relative">
           <Image loading="eager" alt="" width={864} height={576} src={chidi} />
         </div>
+        <div className="hero__scroll p-4">
+          <p className="mr-2">Scroll down</p>
+          <div className="c-infinite">
+            <span className="c-link">
+              <span className="c-link__inner">
+                <span>
+                  <svg
+                    width="20"
+                    height="21"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.833 13.977l4.47-4.47 1.179 1.178L10 17.167l-6.482-6.482 1.179-1.178 4.47 4.47V3.833h1.666v10.144z"
+                      fill="#777"
+                    ></path>
+                  </svg>
+                </span>
+                <span className="c-link__animated">
+                  <svg
+                    width="20"
+                    height="21"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.833 13.977l4.47-4.47 1.179 1.178L10 17.167l-6.482-6.482 1.179-1.178 4.47 4.47V3.833h1.666v10.144z"
+                      fill="#777"
+                    ></path>
+                  </svg>
+                </span>
+              </span>
+            </span>
+          </div>
+        </div>
+      </section>
+      <section>
         <div className="short-description">
           <div className="mb-2">
             I love to build beautiful and highly performant
-            <span className="semi-bold"> applications</span> and{" "}
+            <span className="semi-bold"> applications</span> and
             <span className="semi-bold">interfaces </span>
             using whatever tools are needed. In my free time, I love listening
             to music or playing chess 😊.
